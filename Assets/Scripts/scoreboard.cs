cacb794f97217cb610e702a7a3f7784fe3e13480
KickTheBoot <anoojjonttu@gmail.com>
2023-04-10 20:23:21 +0300

    Add functionality for saving and loading highscores, add procedural scoreboard functionality

--- Assets/Scripts/scoreboard.cs
+++ Assets/Scripts/scoreboard.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 using UnityEngine.UIElements;
 public class scoreboard : MonoBehaviour
 {
-    [SerializeField]
-    VisualElement[] elements;
+    public static scoreboard Instance;
     public TemplateContainer Container;
     UIDocument doc;
+
+    int[] scores;
     // Start is called before the first frame update
     void Start()
     {
+        if(!Instance)
+        Instance = this;
+
         doc = GetComponent<UIDocument>();
-        elements = new TextElement[8];
+        
+    }
 
+    public void ListScores(int[] scores)
+    {
+        VisualElement board = doc.rootVisualElement.Q(name: "ScoreContainer");
 
-        for(int i = 0; i < elements.Length; i++)
+        board.Clear();
+
+        for(int i = 0; i < scores.Length; i++)
         {
+            TextElement score = new TextElement();
+            score.text = $"{i+1}: {scores[i]}";
+            board.Add(score);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
